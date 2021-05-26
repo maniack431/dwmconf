8117a53ffbc0e2fe5f4f6d9bbed1103fb033b9b9
Maniack <59641168+maniack431@users.noreply.github.com>
2021-05-26 20:45:53 +0530

    Update config.def.h

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 static const char *scrotcmd[]  = { "scrot", "-t", "25", NULL };
 static const char *scrotfocusedcmd[]  = { "scrot", "--focused", NULL };
 /*static const char *volupcmd[] = { "mpc", "volume", "+1", NULL };

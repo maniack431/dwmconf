5ac702383807caeb7cb6a99e0a3f8cc90d9d3df1
Maniack <59641168+maniack431@users.noreply.github.com>
2021-05-26 13:33:47 +0530

    Update config.def.h

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -12,13 +12,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12","Font Awesome 5 Free:size=12","Font Awesome 5 Free Solid:size=12","Font Awesome 5 Free Brands:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "Source Code Pro Medium:size=12","Font Awesome 5 Free:size=12","Font Awesome 5 Free Solid:size=12","Font Awesome 5 Free Brands:size=12" };
+static const char dmenufont[]       = "Source Code Pro Medium:size=12";
+static const char col_gray1[]       = "#2E3440";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#FFFFFF";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#7E7E7E";
+static const char col_cyan[]        = "#5E81AC";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
